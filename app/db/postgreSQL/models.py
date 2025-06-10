cb43fb3a3ee6f47de8f3e7240b6520305923852e
Fabien Herry <fabien_herry@yahoo.fr>
2025-06-10 14:45:11 +0200

    ajout du script alembic pour le link des tables avec la base de donnée

--- app/db/postgreSQL/models.py
+++ app/db/postgreSQL/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime,ForeignKey,Date
+from sqlalchemy.orm import relationship
 from .database import Base
 
 #La définition des table postgre via sqlalchemy models
@@ -9,8 +10,12 @@ class HommePolitique(Base):
     prenom=Column(String)
     nom=Column(String)
     date_naissance = Column(Date)
-    proffession=Column(String)
+    profession=Column(String)
+    organes = relationship("OrganeRelation", back_populates="hopol")
+    activites = relationship("Activite",back_populates="hopol")
+    role = relationship("Role",back_populates="hopol")
 
+#L'appartenance a un organe parlementaire
 class Organe(Base):
     __tablename__ = "organe"
     organe_id=Column(String,primary_key=True)
@@ -19,8 +24,31 @@ class Organe(Base):
     code_type = Column(String)
     debut=Column(Date)
     fin=Column(Date)
+    membres = relationship("OrganeRelation" , back_populates="organe")
 
+#Un lien vers une "activité" un vote une participation a un débat ect on retrouve le detail dans la base mongoDB
 class Activite(Base):
     __tablename__="activite"
     activite_id=Column(String,primary_key=True)
+    nom=Column(String)
+    date=Column(Date)
+    type=Column(String)
+    hopol_id = Column(String,ForeignKey("hommepolitique.hopol_id"))
+    hopol = relationship("HommePolitique",back_populates="activites")
 
+class OrganeRelation(Base):
+    __tablename__ = "organe_relation"
+    organe_id=Column(String,ForeignKey("organe.organe_id"),primary_key=True)
+    hopol_id = Column(String,ForeignKey("hommepolitique.hopol_id"),primary_key=True)
+    hopol = relationship("HommePolitique",back_populates="organes")
+    organe = relationship("Organe",back_populates="membres")
+
+#Est ce que c'est un député ou un senateur , quand est qu'il a été élu etc
+class Role(Base):
+    __tablename__="role"
+    id=Column(Integer,primary_key=True)
+    hopol_id = Column(String,ForeignKey("hommepolitique.hopol_id"))
+    role_name = Column(String)
+    date_election=Column(Date)
+    id_election=Column(String)#Un id vers la base mongoDB qui contient l'election 
+    hopol = relationship("HommePolitique",back_populates="role")
